13617f29a9d2ccb29eb7bfd6eb6ab8beef74671d
Bernardo do Amaral Teodosio <berteodosio@gmail.com>
2018-11-15 17:11:04 -0200

    Start good eatins with prototype table cell

---
+++ GoodEatings/GoodEatings/Model/FoodCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FoodCategory.swift
+//  GoodEatings
+//
+//  Created by Bernardo Teodosio on 15/11/18.
+//  Copyright © 2018 Bernardo Teodosio. All rights reserved.
+//
+
+import Foundation
+
+struct FoodCategory {
+    let title: String
+    let imageName: String
+}
---
+++ GoodEatings/GoodEatings/Model/Recipe.swift
@@ -0,0 +1,15 @@
+//
+//  Recipe.swift
+//  GoodEatings
+//
+//  Created by Bernardo Teodosio on 15/11/18.
+//  Copyright © 2018 Bernardo Teodosio. All rights reserved.
+//
+
+import Foundation
+
+struct Recipe {
+    let title: String
+    let instructions: String
+    let imageName: String
+}
