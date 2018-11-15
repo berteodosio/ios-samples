0c0e5f6e5dfd7524bf09d7148cf2f0a5cdf09c5e
Bernardo do Amaral Teodosio <berteodosio@gmail.com>
2018-11-15 15:18:16 -0200

    Adding shows to array; fixed label line wrap

--- TechnologiesBot/TechnologiesBot/ViewController.swift
+++ TechnologiesBot/TechnologiesBot/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var technologiesLabel: UILabel!
+    @IBOutlet weak var showsLabel: UILabel!
+    @IBOutlet weak var responseLabel: UILabel!
+    @IBOutlet weak var chooseShowButton: UIButton!
+    @IBOutlet weak var showTextField: UITextField!
+    
+    private var shows: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        technologiesLabel.isHidden = true
+        showsLabel.isHidden = true
+        responseLabel.isHidden = true
+        chooseShowButton.isHidden = true
     }
 
-
+    @IBAction func onAddShowButtonClick(_ sender: Any) {
+        guard let showText = showTextField.text else { return }
+        shows.append(showText)
+        displayShows()
+        showTextField.text = ""
+    }
+    
+    private func displayShows() {
+        showsLabel.text = shows.joined(separator: ", ")
+        if showsLabel.text?.isEmpty == false {
+            showsLabel.isHidden = false
+            technologiesLabel.isHidden = false
+        }
+    }
+    
 }
 
