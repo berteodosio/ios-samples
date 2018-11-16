cbdac2f9490f77911542293d717fdb07120fec0f
Bernardo do Amaral Teodosio <berteodosio@gmail.com>
2018-11-16 01:36:57 -0200

    Start creation of StarTrivia

---
+++ StarTrivia/StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Bernardo Teodosio on 16/11/18.
+//  Copyright © 2018 Bernardo Teodosio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
